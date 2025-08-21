74b21622006dd5f6de3939e7742213fedb3cf4a4
priyanka-1994 <priyak06111994@gmail.com>
2025-08-21 08:27:02 +0530

    Adding changes

--- src/Content.js
+++ src/Content.js
@@ -33,25 +33,30 @@ const Content = () => {
 
     return (
         <main>
-            <ul>
-                {items.map((item) => (
-                    <li className="item" key={item.id}>
-                        <input 
-                           type="checkbox"
-                           onchange={() => handleCheck(item.id)}
-                           checked={item.checked}
-                        />
-                        <label
-                            style={(item.checked) ? { textDecoration: 'line-through'} : null}
-                            onDoubleClick={()=> handleCheck(item.id)}
-                        >{item.item}</label>
-                        {/* <button>Delete</button> */}
-                        <FaTrashAlt 
-                        onClick={ () => handleDelete(item.id) }
-                        role="button" tabIndex="0"/>
-                    </li>
-                ))}
-            </ul>
+            {items.length ? (
+                <ul>
+                    {items.map((item) => (
+                        <li className="item" key={item.id}>
+                            <input 
+                            type="checkbox"
+                            onchange={() => handleCheck(item.id)}
+                            checked={item.checked}
+                            />
+                            <label
+                                style={(item.checked) ? { textDecoration: 'line-through'} : null}
+                                onDoubleClick={()=> handleCheck(item.id)}
+                            >{item.item}</label>
+                            {/* <button>Delete</button> */}
+                            <FaTrashAlt 
+                            onClick={ () => handleDelete(item.id) }
+                            role="button" tabIndex="0"/>
+                        </li>
+                    ))}
+                 </ul>
+            ) : (
+                <p style={{marginTop: '2rem' }}>Your list is empty</p>
+            )}
+            
         </main>
     )
 }
